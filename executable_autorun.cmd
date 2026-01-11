ce1e0e5e7b091363be82b85443bc35b39c6b470c
Parker Wahle <regulad@regulad.xyz>
2026-01-11 13:11:45 -0500

    Update autorun.cmd

--- executable_autorun.cmd
+++ executable_autorun.cmd
@@ -8,6 +8,9 @@ REM set %VISUAL% for chezmoi & others
 for %%i in (nvim.exe vim.exe) do @if exist "%%~$PATH:i" (set VISUAL=%%~$PATH:i & goto :break)
 :break
 
+REM unix-style prompt while still windows-y
+PROMPT %USERNAME%@%COMPUTERNAME% $P$G
+
 REM just a clean line after clink injects
 REM need to do echo( to not just echo "" or ECHO is off.. boy, I love windows!
 echo(
