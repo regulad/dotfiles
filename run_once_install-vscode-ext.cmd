b9c92b40d447f3d8547210ac8388b3bd92a6508e
Parker Wahle <regulad@regulad.xyz>
2026-01-10 22:14:22 -0500

    force vscode

--- run_once_install-vscode-ext.cmd
+++ run_once_install-vscode-ext.cmd
@@ -2,7 +2,7 @@
 where code >nul 2>nul
 if %ERRORLEVEL% EQU 0 (
     for /F "tokens=*" %%A in (%USERPROFILE%\vscode-extensions.txt) do (
-        call code --install-extension --force %%A
+        call code --install-extension %%A --force
     )
 ) else (
     echo VSCode 'code' command not found in PATH, skipping extension installation
