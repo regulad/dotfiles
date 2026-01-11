46ff0748e4ab7f5f693d471538398d90f2b15cef
Parker Wahle <regulad@regulad.xyz>
2026-01-10 22:11:00 -0500

    Update autorun.cmd

--- executable_autorun.cmd
+++ executable_autorun.cmd
@@ -1,2 +1,14 @@
 @echo off
-"%USERPROFILE%\scoop\apps\clink\current\clink.bat" inject --autorun && doskey /macrofile="%USERPROFILE%\.doskey.mac"
+
+REM call hooks
+call "%USERPROFILE%\scoop\apps\clink\current\clink.bat" inject --autorun
+call doskey /macrofile="%USERPROFILE%\.doskey.mac"
+
+REM set %VISUAL% for chezmoi & others
+for %%i in (nvim.exe vim.exe) do @if exist "%%~$PATH:i" (set VISUAL=%%~$PATH:i & goto :break)
+:break
+
+REM just a clean line after clink injects
+REM need to do echo( to not just echo "" or ECHO is off.. boy, I love windows!
+echo(
+
