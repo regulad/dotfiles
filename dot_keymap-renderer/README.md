e4125f6095df482d02dd53383c737b7ee287ef6f
Parker Wahle <regulad@regulad.xyz>
2026-01-15 17:33:11 -0500

    Update .keymap-renderer/README.md Add .keymap-renderer/__pycache__/key.cpython-314.pyc Add .keymap-renderer/__pycache__/keyboard.cpython-314.pyc Add .keymap-renderer/fonts/GMK_font.ttf Add .keymap-renderer/fonts/SA_font.ttf Add .keymap-renderer/fonts/fa2unicode.json Add .keymap-renderer/fonts/kbd-webfont2unicode.json Add .keymap-renderer/images/GMK_BASE1.png Add .keymap-renderer/images/GMK_BASE2.png Add .keymap-renderer/images/GMK_BASE3.png Add .keymap-renderer/images/GMK_BASE4.png Add .keymap-renderer/images/GMK_BASE5.png Add .keymap-renderer/images/GMK_BIGENTER1.png Add .keymap-renderer/images/GMK_BIGENTER2.png Add .keymap-renderer/images/GMK_BIGENTER3.png Add .keymap-renderer/images/GMK_BIGENTER4.png Add .keymap-renderer/images/GMK_BIGENTER5.png Add .keymap-renderer/images/GMK_ISO1.png Add .keymap-renderer/images/GMK_ISO2.png Add .keymap-renderer/images/GMK_ISO3.png Add .keymap-renderer/images/GMK_ISO4.png Add .keymap-renderer/images/GMK_ISO5.png Add .keymap-renderer/images/GMK_SPACE1.png Add .keymap-renderer/images/GMK_SPACE2.png Add .keymap-renderer/images/GMK_SPACE3.png Add .keymap-renderer/images/GMK_SPACE4.png Add .keymap-renderer/images/GMK_SPACE5.png Add .keymap-renderer/images/GMK_STEP1.png Add .keymap-renderer/images/GMK_STEP2.png Add .keymap-renderer/images/GMK_STEP3.png Add .keymap-renderer/images/GMK_STEP4.png Add .keymap-renderer/images/GMK_STEP5.png Add .keymap-renderer/images/SA_BASE1.png Add .keymap-renderer/images/SA_BASE2.png Add .keymap-renderer/images/SA_BASE3.png Add .keymap-renderer/images/SA_BASE4.png Add .keymap-renderer/images/SA_BASE5.png Add .keymap-renderer/images/SA_BIGENTER1.png Add .keymap-renderer/images/SA_BIGENTER2.png Add .keymap-renderer/images/SA_BIGENTER3.png Add .keymap-renderer/images/SA_BIGENTER4.png Add .keymap-renderer/images/SA_BIGENTER5.png Add .keymap-renderer/images/SA_ISO1.png Add .keymap-renderer/images/SA_ISO2.png Add .keymap-renderer/images/SA_ISO3.png Add .keymap-renderer/images/SA_ISO4.png Add .keymap-renderer/images/SA_ISO5.png Add .keymap-renderer/images/SA_SPACE1.png Add .keymap-renderer/images/SA_SPACE2.png Add .keymap-renderer/images/SA_SPACE3.png Add .keymap-renderer/images/SA_SPACE4.png Add .keymap-renderer/images/SA_SPACE5.png Add .keymap-renderer/images/SA_STEP1.png Add .keymap-renderer/images/SA_STEP2.png Add .keymap-renderer/images/SA_STEP3.png Add .keymap-renderer/images/SA_STEP4.png Add .keymap-renderer/images/SA_STEP5.png Add .keymap-renderer/key.py Add .keymap-renderer/keyboard.py Remove .keymap-renderer/main.py Update .keymap-renderer/pyproject.toml Add .keymap-renderer/render.py Add .keymap-renderer/uv.lock Update .tmux.conf

---
+++ dot_keymap-renderer/README.md
@@ -0,0 +1,19 @@
+# Keymap Renderer
+
+This project takes two files, a [`keyboard-layout-editor`](https://www.keyboard-layout-editor.com/#/) format keyboard description JSON file, and a `.kdl.json` ("keybind description language") file. (JSON schema for a `.kdl.json` file is contained in this project.)
+
+It will then render a keyboard annotated with the bind present in each key in the KDL and produce a Markdown file containing each keybind layer and a table containing additional information.
+
+Each layer's markdown file will then be converted into PDF, and all layers for a given KDL will be merged into a final PDF file. You may then print out this PDF file or use it as reference.
+
+Also contains a script to generate KDLs from the following sources:
+- `nvim-nmap.kdl.json` (create with `uv run nvim-nmap.kdl.py -o /tmp/nvim-nmap.kdl.json`)
+
+## Existing work
+
+Substantial parts of the rendering code for this project are taken from [`kle_render`](https://github.com/CQCumbers/kle_render). Unfortunately, the original keyboard-layout-editor is distributed under an all rights reserved license despite being open source.
+
+## TODO
+
+- Additional KDL creators
+
---
+++ dot_keymap-renderer/render.py
@@ -0,0 +1,60 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from copy import deepcopy
+from json import load
+from typing import Annotated
+
+import typer
+from xdg_base_dirs import xdg_config_home
+from markdown_pdf import MarkdownPdf, Section
+
+from keyboard import Keyboard
+
+
+def main(
+            kdl_file: Annotated[Path, typer.Argument()],
+            kle_file: Annotated[Path, typer.Option()] = xdg_config_home() / "kmr" / "base.json",
+            out: Annotated[Path | None, typer.Option()] = None
+         ) -> None:
+    if out is None:
+        out = kdl_file.with_suffix(".pdf")
+    
+    with TemporaryDirectory() as tempdir:
+        temppath = Path(tempdir)
+        pdf = MarkdownPdf(toc_level=2, optimize=True)
+        
+        with open(kdl_file, "r") as kdl_fp:
+            kdl = load(kdl_fp)
+    
+        # TODO: KDL schema
+    
+        with open(kle_file, mode="r") as kle_fp:
+            # the kle format is actually a js object, not json.
+            kle_deserialized = Keyboard(kle_fp.read())
+    
+        for layer in [True]:
+            this_layer_keyboard = kle_deserialized
+            
+            layer_name = "layer"
+
+            # I'm too used to rust. I keep seeing copying...
+            with this_layer_keyboard.render() as image:
+                image_uri = temppath / f"{layer_name}.png"
+                image.save(image_uri, format="PNG")
+            pdf.add_section(
+                Section(
+                    f"""
+![{layer_name}]({str(image_uri).lstrip("/")})
+
+TODO: extra notes
+
+TODO: bind table
+                    """,
+                    root="/"
+                )
+            ) 
+        pdf.save(out)
+    
+
+if __name__ == "__main__":
+    typer.run(main)
