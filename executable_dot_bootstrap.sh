c7a70459c239b967b33baf4e03a3466ebce021ab
Parker Wahle <regulad@regulad.xyz>
2026-02-06 17:43:34 -0500

    [no ci] Chezmoi auto-commit Update .bootstrap.sh

--- executable_dot_bootstrap.sh
+++ executable_dot_bootstrap.sh
@@ -1,2 +1,4 @@
 #!/opt/homebrew/bin/bash
+# for use with the following bootstrap command
+# /Users/regulad/go/bin/osc52pty /Users/regulad/.bootstrap.sh
 exec /opt/homebrew/bin/tmux new-session -A -s term-${TERM_SESSION_ID:0:8}
