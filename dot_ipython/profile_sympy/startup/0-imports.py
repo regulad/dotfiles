33d291fa3d446bc78f10d12be60eccb7bdd13e47
Parker Wahle <regulad@regulad.xyz>
2026-02-08 18:49:29 -0500

    [no ci] Chezmoi auto-commit Add .ipython/profile_sympy/startup/0-imports.py Add .ipython/profile_sympy/startup/1-constants.py Add .ipython/profile_sympy/startup/README

---
+++ dot_ipython/profile_sympy/startup/0-imports.py
@@ -0,0 +1,13 @@
+# silence warnings
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# real imports
+import math
+import struct
+from math import *
+
+import numpy as np
+from vpython import *
+import statistics as stats
