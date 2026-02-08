63697ce8bf9f9987ed39895384747bd8517a432a
Parker Wahle <regulad@regulad.xyz>
2026-02-08 16:14:35 -0500

    [no ci] Chezmoi auto-commit Update autorun.cmd

--- executable_autorun.cmd
+++ executable_autorun.cmd
@@ -4,16 +4,16 @@ setlocal enabledelayedexpansion
 REM call hooks
 call "%USERPROFILE%\scoop\apps\clink\current\clink.bat" inject --autorun
 
+REM set %VISUAL% for chezmoi & others
+for %%i in (nvim.exe vim.exe) do @if exist "%%~$PATH:i" (set VISUAL=%%~$PATH:i & goto :break)
+:break
+
 REM Check if the clink alias exists (which means Clink is injected)
 where clink >nul 2>&1
 doskey /macros | find /i "clink=" >nul 2>&1
 if not errorlevel 1 (
   REM Clink is injected, so this is an interactive session
 	call doskey /macrofile="%USERPROFILE%\.doskey.mac"
-
-	REM set %VISUAL% for chezmoi & others
-	for %%i in (nvim.exe vim.exe) do @if exist "%%~$PATH:i" (set VISUAL=%%~$PATH:i & goto :break)
-	:break
 
 	REM unix-style prompt while still windows-y
 	PROMPT %USERNAME%@%COMPUTERNAME% $P$G
