deec5d396630aa3aa4c37d5141fc8dc74ff2d91e
Parker Wahle <regulad@regulad.xyz>
2026-02-08 16:33:37 -0500

    [no ci] Chezmoi auto-commit Update autorun.cmd

--- executable_autorun.cmd
+++ executable_autorun.cmd
@@ -4,7 +4,7 @@ setlocal enabledelayedexpansion
 REM call hooks
 call "%USERPROFILE%\scoop\apps\clink\current\clink.bat" inject --autorun
 call doskey /macrofile="%USERPROFILE%\.doskey.mac" 
-set "VISUAL=%USERPROFILE%\scoop\shims\nvim.exe"
+
 
 REM unix-style prompt while still windows-y
 PROMPT %USERNAME%@%COMPUTERNAME% $P$G
@@ -22,3 +22,7 @@ if %errorlevel%==0 (
 	echo(
 )
 
+REM putting the conditional breaks set??? idfk why
+REM set %VISUAL% for chezmoi & others
+for %%i in (nvim.exe vim.exe) do @if exist "%%~$PATH:i" (set VISUAL=%%~$PATH:i & goto :break)
+:break
