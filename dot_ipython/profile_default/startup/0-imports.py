35c3d5aa2f2262358a2b0b80bdf07411e16e6d07
Parker Wahle <regulad@regulad.xyz>
2026-02-08 18:41:02 -0500

    [no ci] Chezmoi auto-commit Update .ipython/profile_default/startup/0-imports.py

--- dot_ipython/profile_default/startup/0-imports.py
+++ dot_ipython/profile_default/startup/0-imports.py
@@ -1,7 +1,4 @@
 import warnings
-
-warnings.filterwarnings("ignore")
-
 import math
 import struct
 from math import *
@@ -10,8 +7,4 @@ import numpy as np
 from vpython import *
 import statistics as stats
 
-# constants
-c = 3e8  # m/s
-G = 6.7e-11  # N * (m^2/kg^2)
-m_e = 9.1e-31  # kg
-m_p = m_n = 1.67e-27  # kg
+warnings.filterwarnings("ignore")
