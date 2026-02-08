1c62f899fb03b05c8eb0ef9ffecfd60b9507ac5e
Parker Wahle <regulad@regulad.xyz>
2026-02-08 18:54:44 -0500

    [no ci] Chezmoi auto-commit Update .ipython/profile_sympy/startup/0-imports.py

--- dot_ipython/profile_sympy/startup/0-imports.py
+++ dot_ipython/profile_sympy/startup/0-imports.py
@@ -3,11 +3,41 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-# real imports
-import math
-import struct
-from math import *
+# https://docs.sympy.org/latest/install.html#mpmath-installation
+import sys
+import mpmath
 
-import numpy as np
-from vpython import *
-import statistics as stats
+sys.modules["sympy.mpmath"] = mpmath
+
+# real imports
+from sympy import *
+
+init_printing(use_unicode=True)  # my terminals can handle it
+# every key on my ti89
+a = Symbol("a")
+b = Symbol("b")
+c = Symbol("c")
+d = Symbol("d")
+e = Symbol("e")
+f = Symbol("f")
+g = Symbol("g")
+h = Symbol("h")
+i = Symbol("i")
+j = Symbol("j")
+k = Symbol("k")
+l = Symbol("l")
+m = Symbol("m")
+n = Symbol("n")
+o = Symbol("o")
+p = Symbol("p")
+q = Symbol("q")
+r = Symbol("r")
+s = Symbol("s")
+t = Symbol("t")
+u = Symbol("u")
+v = Symbol("v")
+w = Symbol("w")
+x = Symbol("x")
+y = Symbol("y")
+z = Symbol("z")
+theta = Symbol("theta")
