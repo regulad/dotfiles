5d8456cc418f792bc760e5f9ce5234e4b1fa5e1a
Parker Wahle <regulad@regulad.xyz>
2026-02-08 16:26:29 -0500

    [no ci] Chezmoi auto-commit Update autorun.cmd

--- executable_autorun.cmd
+++ executable_autorun.cmd
@@ -5,9 +5,8 @@ REM call hooks
 call "%USERPROFILE%\scoop\apps\clink\current\clink.bat" inject --autorun
 call doskey /macrofile="%USERPROFILE%\.doskey.mac"
 
-REM set VISUAL for chezmoi
-where nvim.exe >nul 2>&1 && for /f "delims=" %i in ('where nvim.exe') do set "VISUAL=%i" && goto :break
-where vim.exe >nul 2>&1 && for /f "delims=" %i in ('where vim.exe') do set "VISUAL=%i" && goto :break
+REM set visual
+for %i in (nvim.exe vim.exe) do @where %i >nul 2>&1 && for /f "delims=" %p in ('where %i') do set "VISUAL=%p" & goto :break
 :break
 
 REM Check if the clink alias exists (which means Clink is injected)
