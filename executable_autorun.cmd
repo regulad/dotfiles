e06c41f2efaeb200bb1a6ec18eaf5414bc4695b8
Parker Wahle <regulad@regulad.xyz>
2026-02-08 16:34:17 -0500

    [no ci] Chezmoi auto-commit Update autorun.cmd

--- executable_autorun.cmd
+++ executable_autorun.cmd
@@ -9,19 +9,6 @@ call doskey /macrofile="%USERPROFILE%\.doskey.mac"
 REM unix-style prompt while still windows-y
 PROMPT %USERNAME%@%COMPUTERNAME% $P$G
 
-REM Check if the clink alias exists (which means Clink is injected)
-where clink >nul 2>&1
-call clink info 2>nul | find /i "injected" >nul 2>&1
-if %errorlevel%==0 (
-	REM final fastfetch for both terminal-porn and usefulness since i switch between machines
-	echo(
-	call fastfetch
-
-	REM just a clean line after clink injects
-	REM need to do echo( to not just echo "" or ECHO is off.. boy, I love windows!
-	echo(
-)
-
 REM putting the conditional breaks set??? idfk why
 REM set %VISUAL% for chezmoi & others
 for %%i in (nvim.exe vim.exe) do @if exist "%%~$PATH:i" (set VISUAL=%%~$PATH:i & goto :break)
