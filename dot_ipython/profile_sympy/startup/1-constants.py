5854aa446a8ac92ce340ffa48c056668111c7a38
Parker Wahle <regulad@regulad.xyz>
2026-02-08 19:24:41 -0500

    [no ci] Chezmoi auto-commit Update .ipython/profile_sympy/startup/1-constants.py

--- dot_ipython/profile_sympy/startup/1-constants.py
+++ dot_ipython/profile_sympy/startup/1-constants.py
@@ -1,4 +1,31 @@
-c = 3e8  # m/s
-G = 6.7e-11  # N * (m^2/kg^2)
+# copied from https://apcentral.collegeboard.org/media/pdf/ap-physics-2-equations-sheet.pdf
+# Avogadro's number
+N_0 = 6.02e23  # mol^-1
+# Universal gas constant
+R = 8.31  # J/(mol * K)
+# Boltzmann's constant
+k_B = 1.38e-23  # J/K
+# Coulomb constant
+k = 9.0e9  # (N * m^2)/(C^2)
+# Proton mass
+m_p = 1.67e-27  # kg
+# Neutron mass
+m_n = 1.67e-27  # kg
+# Electron mass
 m_e = 9.1e-31  # kg
-m_p = m_n = 1.67e-27  # kg
+# Elementary charge
+e = 1.60e-19  # C
+# Vacuum permittivity
+epsilon_0 = 8.85e-12  # C^2/(N*m^2)
+# Vacuum permeability
+mu_0 = (4 * pi) * 1e-7  # (T * m)/A
+# Planck's constant
+h = 6.63e-34  # J*s
+# Speed of light
+c = 3.00e8  # m/s
+# Wien's constant
+b = 2.90e-3  # m*K
+# Stefan-Boltzmann constant
+sigma = 5.67e-8  # W/(m^2 * K^4)
+# Universal gravitational constant
+G = 6.7e-11  # N * (m^2/kg^2)
