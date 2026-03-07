8ffc09ac1692a272a6ef3372a5eecedbd9b4ca13
Parker Wahle <regulad@regulad.xyz>
2026-03-06 19:58:56 -0500

    [no ci] Chezmoi auto-commit Update .chezmoiscripts/01-darwin/01-install-vscode-ext.sh

--- .chezmoiscripts/01-darwin/run_after_01-install-vscode-ext.sh
+++ .chezmoiscripts/01-darwin/run_after_01-install-vscode-ext.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-# DEP_HASH:72b0187b068f6c39d4337b413e587875ac18b6e058f2b2ae41c5102f595dc95c
-
 if command -v code &>/dev/null; then
     # for some reason, on my machine vscode's --install-extension cli is privvy to fail intermittently
     retry -t 5 -d 2 -m 60 -- sh -c 'cat ~/.vscode-extensions.txt | xargs -I {} code --install-extension {} --force'
