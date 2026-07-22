e0868dda5dbd25cc9c8a77e134d7fffd73176016
Parker Wahle <regulad@regulad.xyz>
2026-07-21 22:18:59 -0400

    [no ci] NT cargo

---
+++ .chezmoiscripts/00-nt/run_after_55-cargo.cmd
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if defined CARGO_HOME (
+    set "CARGO_BIN=%CARGO_HOME%\bin"
+) else (
+    set "CARGO_BIN=%USERPROFILE%\.cargo\bin"
+)
+
+echo ;%PATH%; | find /I ";%CARGO_BIN%;" >nul
+if errorlevel 1 (
+    setx PATH "%PATH%;%CARGO_BIN%"
+    echo Added %CARGO_BIN% to PATH.
+) else (
+    echo %CARGO_BIN% is already in PATH.
+)
+
+call refreshenv >nul 2>&1
+
+call cargo install cargo-disasm
+call cargo install vtracer 
