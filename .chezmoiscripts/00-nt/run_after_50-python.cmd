8a87f837ba198eaac2840a93d53a8e6bc8bf8c3c
Parker Wahle <regulad@regulad.xyz>
2026-08-01 15:59:13 -0400

    [no ci] Chezmoi auto-commit Update .chezmoiscripts/00-nt/50-python.cmd

--- .chezmoiscripts/00-nt/run_after_50-python.cmd
+++ .chezmoiscripts/00-nt/run_after_50-python.cmd
@@ -3,12 +3,15 @@ setlocal enabledelayedexpansion
 
 echo note: installing python tooling
 REM matches unix script
+
 uv tool install -q ruff
 uv tool install -q ty
+uv tool install -q hf
 uv tool install -q pre-commit
 uv tool install --python cp310 -q "git+https://github.com/regulad/keymap-renderer.git@master"
 uv tool install --python cp314 -q "rendercv[full]@2.3"
 uv tool install -q hatch
 uv tool install -q autopep8
-uv tool install -q "yt-dlp[default]"
+uv tool install --python cp314 -q "yt-dlp[default]"
+
 uv tool upgrade --quiet --all
