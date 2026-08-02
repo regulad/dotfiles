876f21f3f60dccd0a192d9e2a83421e6825288f7
Parker Wahle <regulad@regulad.xyz>
2026-08-02 01:31:27 -0400

    [no ci] Chezmoi auto-commit Update .chezmoiscripts/00-nt/02-x509.cmd

--- .chezmoiscripts/00-nt/run_once_after_02-x509.cmd
+++ .chezmoiscripts/00-nt/run_once_after_02-x509.cmd
@@ -1,8 +1,9 @@
 @echo off
 REM installing certificate
 echo debug: installing custom certificate(s)
+REM home.arpa
 sudo certutil -addstore "Root" "%USERPROFILE%\.x509\ipa-ca.crt"
+REM regulad.internal
+sudo certutil -addstore "Root" "%USERPROFILE%\.x509\regulad.internal.crt"
+REM gonkputer nonspecific
 sudo certutil -addstore "Root" "%USERPROFILE%\.x509\pve-root-ca.pem"
-
-sudo certutil -addstore "Root" "%USERPROFILE%\.x509\GUI+default+(66c76753e5636).crt"
-sudo certutil -addstore "Root" "%USERPROFILE%\.x509\GUI+default+(6a6907bb2c8b7).crt"
