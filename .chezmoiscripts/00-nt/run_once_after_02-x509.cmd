64c0c5349ce60d57545b9a701b5d7da0ad182118
Parker Wahle <regulad@regulad.xyz>
2026-08-02 20:41:29 -0400

    [no ci] Chezmoi auto-commit Update .chezmoiscripts/00-nt/02-x509.cmd

--- .chezmoiscripts/00-nt/run_once_after_02-x509.cmd
+++ .chezmoiscripts/00-nt/run_once_after_02-x509.cmd
@@ -5,5 +5,7 @@ REM home.arpa
 sudo certutil -addstore "Root" "%USERPROFILE%\.x509\ipa-ca.crt"
 REM regulad.internal
 sudo certutil -addstore "Root" "%USERPROFILE%\.x509\regulad.internal.crt"
+REM cohensstore003.internal
+sudo certutil -addstore "Root" "%USERPROFILE%\.x509\cohensstore003.internal.crt"
 REM gonkputer nonspecific
 sudo certutil -addstore "Root" "%USERPROFILE%\.x509\pve-root-ca.pem"
